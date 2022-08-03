c7e17bd2fd61e00dd88c5f83deab8ef13108c501
tito <elton.shiau@gmail.com>
2022-08-03 11:55:27 -0300

    1_starter.py

---
+++ projeto_1/1_starter.py
@@ -0,0 +1,12 @@
+def main():
+    # Feel free to add more songs to this list
+    rankings = ["Fearless", "Red", "Long Live"]
+
+    # CODE GOES HERE, after this line
+
+    return
+
+
+# Don't worry about this for now, it basically runs the code above
+if __name__ == '__main__':
+    main()
